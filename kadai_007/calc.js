5f168115298d68809421d01e7ce049685b2eb94a
yousuke-sss <you.sekine130702@outlook.jp>
2024-08-27 23:03:27 +0900

    kadai_007

--- kadai_007/calc.js
+++ kadai_007/calc.js
@@ -1,18 +1,19 @@
 let num = Math.floor(Math.random() * 20);
 
-  if (num % 3 == 0) {
-    console.log('3の倍数です');
-  }
+  if (num % 3 == 0 && num % 5 == 0) {
+    console.log('3と5の倍数です');
+  }  
 
   else if (num % 5 == 0) {
     console.log('5の倍数です');
   }  
 
-  if (num % 3 == 0 && num % 5 == 0) {
-    console.log('3と5の倍数です');
-  }  
+  else if (num % 3 == 0) {
+    console.log('3の倍数です');
+  }
 
   else {
     console.log(num);
   } 
 
+  
