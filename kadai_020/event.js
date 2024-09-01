9dc99ac7b72f621c5098c43ae33b8db403362527
yousuke-sss <you.sekine130702@outlook.jp>
2024-09-01 20:03:04 +0900

    kadai_020

---
+++ kadai_020/event.js
@@ -0,0 +1,11 @@
+// btn idを取得
+const Btn = document.getElementById('btn');
+const Text = document.getElementById('text');
+
+// btn id を持つHTML要素をクリックしたときにイベント処理を実行
+Btn.addEventListener('click' , () => {
+
+   Text.textContent = 'ボタンをクリックしました';
+   
+});
+
